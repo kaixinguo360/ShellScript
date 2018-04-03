6d4d59c425094d7c21d2b1a204d8d9544ae29111
Kaixinguo360 <kaixinguo360@gmail.com>
2018-04-03 16:25:31 +0800

    Update lnmp.sh

--- lnmp/lnmp.sh
+++ lnmp/lnmp.sh
@@ -5,11 +5,11 @@ export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
 [ $(id -u) != "0" ] && { echo "Error: You must be root to run this script"; exit 1; }
 
 # 检查系统信息
-if [ ! -z "`cat /etc/issue | grep 'Ubuntu 16.04'`" ];
+if [ ! -z "`cat /etc/issue | grep 'Ubuntu 16'`" ];
     then
         OS='Ubuntu'
     else
-        echo "Not support OS(Ubuntu 16.04), Please reinstall OS and retry!"
+        echo "Not support OS(Ubuntu 16), Please reinstall OS and retry!"
         #exit 1
 fi
 
