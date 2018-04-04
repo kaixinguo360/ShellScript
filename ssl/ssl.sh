44a602d9a27061da1894af11caaea2d7afa9d633
Kaixinguo360 <kaixinguo360@gmail.com>
2018-04-04 12:39:18 +0800

    Create ssl.sh

---
+++ ssl/ssl.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+
+# 检查是否为Root
+[ $(id -u) != "0" ] && { echo "Error: You must be root to run this script"; exit 1; }
+
+# 检查系统信息
+if [ ! -z "`cat /etc/issue | grep 'Ubuntu 16'`" ];
+    then
+        OS='Ubuntu'
+    else
+        echo "Not support OS(Ubuntu 16), Please reinstall OS and retry!"
+        #exit 1
+fi
+
+
+# 正式安装开始
+
+# 设置变量
+NGINX_CONF='/etc/nginx/sites-available/default'
+
+# 读取参数
+
+read -p '您的网站域名: ' SERVER_NAME
