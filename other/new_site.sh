de9f8f22deecd032267556d2bb5487b17272913f
Kaixinguo360 <kaixinguo360@gmail.com>
2018-04-05 18:20:05 +0800

    Update new_site.sh

--- other/new_site.sh
+++ other/new_site.sh
@@ -19,12 +19,32 @@ fi
 # 设置变量
 NGINX_CONF='/etc/nginx/sites-enabled/'
 MY_CONF='/etc/nginx/my/'
+SSL_PATH='/etc/nginx/ssl/'
 
 # 读取参数
 read -p '新网站的本地配置文件名: ' SITE_NAME
 read -p '新网站的根目录: ' SITE_ROOT
 read -p '新网站的本地监听端口: ' SITE_PORT
-read -p '新网站的域名: ' HOST_NAME
+read -p '新网站的域名: ' SERVER_NAME
+while true :
+do
+	read -r -p "启用SSL? [Y/n] " input
+
+	case $input in
+	    [yY][eE][sS]|[yY])
+	    		ENABLE_SSL='1'
+			break
+            		;;
+
+	    [nN][oO]|[nN])
+            		break
+            		;;
+
+	    *)
+		echo "Invalid input..."
+		;;
+	esac
+done
 
 # 新建配置文件
 cat > ${NGINX_CONF}${SITE_NAME} << HERE
@@ -40,7 +60,7 @@ server {
 	# Add index.php to the list if you are using PHP
 	index index.php index.html index.htm index.nginx-debian.html;
 
-	server_name ${HOST_NAME};
+	server_name ${SERVER_NAME};
 
 	client_max_body_size 20m;
 
@@ -85,3 +105,36 @@ mkdir -p ${MY_CONF}${SITE_NAME}
 # 重启Server
 service nginx restart
 
+
+## 开启SSL ##
+
+if [ -n "${ENABLE_SSL}" ]; then
+
+#安装 acme.sh 以自动获取SSL证书
+ACME="${HOME}/.acme.sh/acme.sh"
+if [ ! -x ${ACME} ]; then
+    su - $SUDO_USER -c "curl  https://get.acme.sh | sh" && source "${HOME}/.acme.sh/acme.sh.env"
+fi
+
+# 获取SSL证书
+${ACME} --issue  -d  ${SERVER_NAME}  --nginx || exit -1
+
+# 安装SSL证书
+mkdir -p ${SSL_PATH}
+${ACME}  --installcert  -d  ${SERVER_NAME} \
+        --key-file  ${SSL_PATH}${SERVER_NAME}.key \
+        --fullchain-file  ${SSL_PATH}fullchain.cer \
+        --reloadcmd  "service nginx force-reload" || exit -1
+
+# 配置Nginx
+cat > ${MY_CONF}${SITE_NAME} << HERE
+listen 443 ssl;
+listen \[::\]:443 ssl;
+ssl_certificate ${SSL_PATH}fullchain.cer;
+ssl_certificate_key ${SSL_PATH}${SERVER_NAME}.key;
+keepalive_timeout   70;
+HERE
+
+# 重启Server
+service nginx restart
+fi
