21302053e2e76b0caf27ad612b0fce86e51fde9b
Kaixinguo360 <kaixinguo360@gmail.com>
2018-04-07 01:34:35 +0800

    Update owncloud.sh

--- owncloud/owncloud.sh
+++ owncloud/owncloud.sh
@@ -26,6 +26,43 @@ NEW_SITE_URL="https://raw.githubusercontent.com/kaixinguo360/ShellScript/master/
 # 读取用户输入
 read -p '您的网站域名: ' SERVER_NAME
 
+while true :
+do
+    read -r -p "创建新的MySQL用户? [Y/n] " input
+
+    case $input in
+        [yY][eE][sS]|[yY])
+            CREATE_USER='1'
+            break
+            ;;
+
+	    [nN][oO]|[nN])
+            break
+            ;;
+
+        *)
+        echo "Invalid input..."
+        ;;
+    esac
+done
+
+if [ -n "${CREATE_USER}" ];then
+read -s -p '请输入MySQL根用户密码(!务必正确!): ' MYSQL_PW
+read -p '请设置新的MySQL用户名: ' MYSQL_USER
+while true :
+do
+    read -s -p "请设置MySQL用户 ${MYSQL_USER} 的密码: " MYSQL_PASSWORD_1
+    echo ''
+    read -s -p '再输一遍: ' MYSQL_PASSWORD_2
+    echo ''
+    if [ "${MYSQL_PASSWORD_1}" = "${MYSQL_PASSWORD_2}" ]; then
+        MYSQL_PW=${MYSQL_PASSWORD_1}
+        break
+    else
+        echo -e "两次输入密码不一致!\n"
+    fi
+done
+fi
 
 ## 正式安装开始 ##
 
@@ -80,7 +117,23 @@ sed -i "s/TMP_SERVER_NAME/${SERVER_NAME}/g" ${NGINX_CONF}owncloud
 service nginx restart
 
 
+## 创建MySQL用户 ##
 
+if [ -n "${CREATE_USER}" ];then
+
+# 数据库操作
+MYSQL_SHORTCUT="mysql -u root -p${MYSQL_PW} -e"
+# 创建数据库
+${MYSQL_SHORTCUT} "CREATE DATABASE oc DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci;"
+# 创建WP用户
+${MYSQL_SHORTCUT} "GRANT ALL ON oc.* TO '${MYSQL_USER}'@'localhost' IDENTIFIED BY '${MYSQL_PW}';"
+# 刷新特权, 令MySQL知道最近的改变:
+${MYSQL_SHORTCUT} "FLUSH PRIVILEGES;"
+
+fi
+
+echo -e "\n  ## OwnCloud安装完成 ##"
+echo -e "   您可以通过 http://${SERVER_NAME}/ 访问OwnCloud\n"
 
 
 
