138e6cf4ac7b975e8a3a6280d6ab0d29e23d1344
Kaixinguo360 <kaixinguo360@gmail.com>
2018-04-07 00:44:00 +0800

    Update owncloud.sh

--- owncloud/owncloud.sh
+++ owncloud/owncloud.sh
@@ -17,7 +17,10 @@ fi
 ## 初始化安装参数 ##
 
 # 设置静态变量
+APT_KEY='https://download.owncloud.org/download/repositories/production/Ubuntu_16.04/Release.key'
+APT_SOURCE='http://download.owncloud.org/download/repositories/production/Ubuntu_16.04/'
 NGINX_CONF='/etc/nginx/sites-enabled/'
+OC_SITE_CONF='https://raw.githubusercontent.com/kaixinguo360/ShellScript/master/owncloud/nginx_site_conf'
 
 # 读取用户输入
 read -p '您的网站域名: ' SERVER_NAME
@@ -38,16 +41,33 @@ done
 ## 正式安装开始 ##
 
 # 新增apt密钥
-wget -nv https://download.owncloud.org/download/repositories/production/Ubuntu_16.04/Release.key -O Release.key
+wget -nv ${APT_KEY} -O Release.key
 apt-key add - < Release.key
 rm -rf Release.key
 
 # 更新apt
-echo 'deb http://download.owncloud.org/download/repositories/production/Ubuntu_16.04/ /' > /etc/apt/sources.list.d/owncloud.list
+echo "deb ${APT_SOURCE} /" > /etc/apt/sources.list.d/owncloud.list
 apt-get update
+
+# 安装OwnCloud
 apt-get install owncloud-files -y
 
-# 配置Nginx
+# 安装PHP扩展插件
+apt-get install php-curl php-gd php-mbstring php-mcrypt php-xml php-xmlrpc php-zip php-intl -y
+systemctl restart php7.0-fpm
+
+
+## 配置Nginx ##
+
+# 下载配置文件
+wget -O ${NGINX_CONF}owncloud ${OC_SITE_CONF}
+
+# 修改配置文件
+sed -i "s/TMP_SERVER_NAME/${SERVER_NAME}/g" ${NGINX_CONF}owncloud
+
+
+
+
 
 
 
