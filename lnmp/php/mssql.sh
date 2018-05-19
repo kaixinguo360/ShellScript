2f8d50edb247fef79589bfe179490ab74f03d23d
Kaixinguo360 <kaixinguo360@gmail.com>
2018-05-19 19:06:31 +0800

    Create mssql.sh

---
+++ lnmp/php/mssql.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+export PATH=/usr/local/sbin:/usr/local/bin:/sbin:/bin:/usr/sbin:/usr/bin
+
+# 检查是否为Root
+[ $(id -u) != "0" ] && { echo "Error: You must be root to run this script"; exit 1; }
+
+# 检查系统信息
+if [ ! -z "`cat /etc/issue | grep 'Ubuntu 16'`" ];
+    then
+        OS='Ubuntu'
+    else
+        echo "Not support OS(Ubuntu 16), Please reinstall OS and retry!"
+        #exit 1
+fi
+
+## 初始化安装参数 ##
+
+# 设置静态变量
+PHP_CONF='/etc/php/7.0/fpm/php.ini'
+
+## 正式安装开始 ##
+apt-get update
+
+# 安装完整php
+apt install -y php7.0 php7.0-dev php7.0-xml php-pear
+
+# 安装 Linux 和 macOS 上的 Microsoft ODBC Driver for SQL Server
+curl https://packages.microsoft.com/keys/microsoft.asc | apt-key add 
+curl https://packages.microsoft.com/config/ubuntu/16.04/prod.list > /etc/apt/sources.list.d/mssql-release.list
+apt-get update
+sudo ACCEPT_EULA=Y apt-get install msodbcsql17
+sudo ACCEPT_EULA=Y apt-get install mssql-tools
+echo 'export PATH="$PATH:/opt/mssql-tools/bin"' >> ~/.bashrc
+source ~/.bashrc
+sudo apt-get install unixodbc-dev
+
+# 安装php插件
+pecl install sqlsrv
+pecl install pdo_sqlsrv
+sudo bash -c 'echo extension=pdo_sqlsrv.so >> `php --ini | grep "Scan for additional .ini files" | sed -e "s|.*:\s*||"`/30-pdo_sqlsrv.ini'
+sudo bash -c 'echo extension=sqlsrv.so >> `php --ini | grep "Scan for additional .ini files" | sed -e "s|.*:\s*||"`/20-sqlsrv.ini'
