b07a47bc3b810bfc8a3c3fa1e296358af1e895d2
Kaixinguo360 <kaixinguo360@gmail.com>
2018-06-27 12:43:03 +0800

    Update restore.sh

--- backup/restore.sh
+++ backup/restore.sh
@@ -111,6 +111,7 @@ if [ -n "$URL" ];then
 fi
 
 if [[ $FILE != "/tmp/backup/backup.tar.gz" && -n "$ENABLE_COPY" ]]; then
+    mkdir /tmp/backup/
     cp $FILE "/tmp/backup/backup.tar.gz"
     FILE="/tmp/backup/backup.tar.gz"
 fi
